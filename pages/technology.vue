566e23245a42ada34fa4df89396f0e0ae60b1f23
Felix Tesche <felix.tesche@gmail.com>
2023-06-09 15:47:29 +0200

    first raw categories list

--- pages/technology.vue
+++ pages/technology.vue
@@ -19,7 +19,7 @@ const { data: technologyData } = await useKql({
       query: 'page.technologies.toStructure',
       select: {
         name: true,
-        description: true,
+        description: 'structureItem.description.toBlocks',
         image: {
           query: 'structureItem.image.toFile',
           select: {
@@ -45,42 +45,75 @@ const { data: technologyData } = await useKql({
     },
   },
 })
+
+const { data: categoryData } = await useKql({
+  query: 'page("technology")',
+  select: {
+    categories: {
+      query: 'page.categories.toStructure',
+      select: {
+        name: true,
+      },
+    },
+  },
+})
 </script>
 
 <template>
   <div>
-    {{ technologyData }}
+    <!-- {{ technologyData }} -->
     <h1 class="font-headline text-large-title font-bold">{{ page.title }}</h1>
+    <div class="flex flex-row">
+      <div class="category-list sticky top-0 w-2/12">
+        <h2>All Available Categories</h2>
+        <ul>
+          <li
+            v-for="(category, index) in categoryData?.result.categories"
+            :key="index"
+          >
+            {{ category.name }}
+          </li>
+        </ul>
+      </div>
 
-    <div class="tech-grid">
-      <div
-        v-for="(tech, index) in technologyData?.result.technologies"
-        :key="index"
-        class="tech-item"
-      >
-        <div class="flex flex-row">
-          <div class="left-side w-1/2">
-            <h2 class="font-headline text-title-1">{{ tech.name }}</h2>
-            <img
-              :src="tech.image.url"
-              :srcset="tech.image.srcset"
-              :alt="tech.name"
-              class="w-72"
-            />
+      <div class="tech-grid w-10/12">
+        <div
+          v-for="(tech, index) in technologyData?.result.technologies"
+          :key="index"
+          class="tech-item"
+        >
+          <div class="flex flex-row">
+            <div class="left-side w-1/2">
+              <h2 class="font-headline text-large-title">{{ tech.name }}</h2>
+              <img
+                :src="tech.image.url"
+                :srcset="tech.image.srcset"
+                :alt="tech.name"
+                class="w-72"
+              />
+            </div>
+            <div class="right-side w-1/2">
+              <KirbyBlocks :blocks="tech.description ?? []" class="note text" />
+            </div>
           </div>
-          <div class="right-side w-1/2">
-            <p class="font-paragraph text-body">{{ tech.description }}</p>
+          <div class="flex flex-row">
+            <div
+              v-for="(device, index) in tech.devices"
+              :key="index"
+              class="w-1/4"
+            >
+              <h2 class="font-headline text-title-1 text-center">
+                {{ device.title }}
+              </h2>
+              <img
+                v-if="device.image"
+                :src="device.image.url"
+                :srcset="device.image.srcset"
+                :alt="device.name"
+                class="w-54"
+              />
+            </div>
           </div>
-        </div>
-        <div v-for="(device, index) in tech.devices" :key="index">
-          <h2 class="font-headline text-title-1">{{ device.title }}</h2>
-          <img
-            v-if="device.image"
-            :src="device.image.url"
-            :srcset="device.image.srcset"
-            :alt="device.name"
-            class="w-36"
-          />
         </div>
       </div>
     </div>
@@ -88,6 +121,10 @@ const { data: technologyData } = await useKql({
 </template>
 
 <style scoped>
-.test {
+.category-list {
+  position: sticky;
+  top: 12rem;
+  height: 100vh; /* adjust this value as needed */
+  overflow-y: auto;
 }
 </style>
