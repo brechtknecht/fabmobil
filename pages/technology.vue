a350601967d9d56d02b576e275c2522565b41df6
Felix Tesche <felix.tesche@gmail.com>
2023-06-09 20:09:39 +0200

    updated theming on technology page

--- pages/technology.vue
+++ pages/technology.vue
@@ -1,3 +1,76 @@
+<template>
+  <section class="bg-secondary">
+    <div class="container mx-auto px-4 sm:px-6 lg:px-8 max-w-7xl">
+      <div>
+        <!-- {{ technologyData }} -->
+        <div class="flex flex-row">
+          <div class="category-list sticky top-0 w-4/12">
+            <h1 class="font-headline text-primary text-large-title font-bold">
+              {{ page.title }}
+            </h1>
+            <h2 class="font-headline text-primary">All Available Categories</h2>
+            <ul>
+              <li
+                v-for="(category, index) in categoryData?.result.categories"
+                :key="index"
+                class="font-paragraph text-primary"
+              >
+                {{ category.name }}
+              </li>
+            </ul>
+          </div>
+
+          <div class="tech-grid w-8/12 pt-16">
+            <div
+              v-for="(tech, index) in technologyData?.result.technologies"
+              :key="index"
+              class="tech-item"
+            >
+              <div class="flex flex-row">
+                <div class="left-side w-1/2">
+                  <h2 class="font-headline text-primary text-large-title">
+                    {{ tech.name }}
+                  </h2>
+                  <img
+                    :src="tech.image.url"
+                    :srcset="tech.image.srcset"
+                    :alt="tech.name"
+                    class="w-72"
+                  />
+                </div>
+                <div class="right-side w-1/2">
+                  <KirbyBlocks
+                    :blocks="tech.description ?? []"
+                    class="note text text-primary"
+                  />
+                </div>
+              </div>
+              <div
+                class="devices pt-20 pb-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4"
+              >
+                <div v-for="(device, index) in tech.devices" :key="index">
+                  <h2
+                    class="font-headline text-primary text-title-1 text-center"
+                  >
+                    {{ device.title }}
+                  </h2>
+                  <img
+                    v-if="device.image"
+                    :src="device.image.url"
+                    :srcset="device.image.srcset"
+                    :alt="device.name"
+                    class="w-54"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
 <script setup lang="ts">
 const { data } = await useKql({
   query: 'page("technology")',
@@ -58,67 +131,6 @@ const { data: categoryData } = await useKql({
   },
 })
 </script>
-
-<template>
-  <div>
-    <!-- {{ technologyData }} -->
-    <h1 class="font-headline text-large-title font-bold">{{ page.title }}</h1>
-    <div class="flex flex-row">
-      <div class="category-list sticky top-0 w-2/12">
-        <h2>All Available Categories</h2>
-        <ul>
-          <li
-            v-for="(category, index) in categoryData?.result.categories"
-            :key="index"
-          >
-            {{ category.name }}
-          </li>
-        </ul>
-      </div>
-
-      <div class="tech-grid w-10/12">
-        <div
-          v-for="(tech, index) in technologyData?.result.technologies"
-          :key="index"
-          class="tech-item"
-        >
-          <div class="flex flex-row">
-            <div class="left-side w-1/2">
-              <h2 class="font-headline text-large-title">{{ tech.name }}</h2>
-              <img
-                :src="tech.image.url"
-                :srcset="tech.image.srcset"
-                :alt="tech.name"
-                class="w-72"
-              />
-            </div>
-            <div class="right-side w-1/2">
-              <KirbyBlocks :blocks="tech.description ?? []" class="note text" />
-            </div>
-          </div>
-          <div class="flex flex-row">
-            <div
-              v-for="(device, index) in tech.devices"
-              :key="index"
-              class="w-1/4"
-            >
-              <h2 class="font-headline text-title-1 text-center">
-                {{ device.title }}
-              </h2>
-              <img
-                v-if="device.image"
-                :src="device.image.url"
-                :srcset="device.image.srcset"
-                :alt="device.name"
-                class="w-54"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</template>
 
 <style scoped>
 .category-list {
