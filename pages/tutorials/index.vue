134c317b58f6b9d37f1dbd81d5e449b8590e5c18
Felix Tesche <felix.tesche@gmail.com>
2023-06-28 15:17:28 +0200

    added parallax effect for materials

--- pages/tutorials/index.vue
+++ pages/tutorials/index.vue
@@ -24,7 +24,7 @@
     <transition-group
       name="fade"
       tag="ul"
-      class="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-4"
+      class="grid grid-cols-4 sm:grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-4 p-8"
     >
       <li
         v-for="(item, index) in filteredTutorials"
