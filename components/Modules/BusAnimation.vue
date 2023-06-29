8ef29c13c5d9d99fd49eb4ceba2780f6eac691d8
Felix Tesche <felix.tesche@gmail.com>
2023-06-29 20:52:16 +0200

    tracking of video position is now possible

--- components/Modules/BusAnimation.vue
+++ components/Modules/BusAnimation.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { defineComponent } from 'vue'
+import { defineComponent, onBeforeUnmount, onMounted, ref } from 'vue'
 import ScrollyVideo from 'scrolly-video/dist/ScrollyVideo.vue'
 
 const Component = defineComponent({
@@ -7,17 +7,54 @@ const Component = defineComponent({
     ScrollyVideo,
   },
 })
+
+// Create a ref for ScrollyVideo
+const scrollyVideo = ref(null)
+
+const handleScroll = () => {
+  // Access ScrollyVideo's properties here
+  console.log('ScrollyVideo props', scrollyVideo.value.scrollyVideo.currentTime)
+  const scrollyVideoRef = scrollyVideo.value.scrollyVideo
+  const calculateCurrentTimePercentage = (scrollyVideoRef) => {
+    // Calculate the total duration of the video in seconds
+    const totalDuration =
+      scrollyVideoRef.frames.length / scrollyVideoRef.frameRate
+
+    // Calculate the percentage of the current time
+    const currentTimePercentage =
+      (scrollyVideoRef.currentTime / totalDuration) * 100
+
+    return currentTimePercentage
+  }
+
+  // Use the function
+  const percentage = calculateCurrentTimePercentage(
+    scrollyVideo.value.scrollyVideo
+  )
+  console.log('Current Time Percentage: ', percentage)
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onBeforeUnmount(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
   <div class="section bg-black">
     <div class="container mx-auto">
       <div class="scrolly-video-container">
+        <!-- Assign the ref here -->
         <ScrollyVideo
+          ref="scrollyVideo"
           class="relative p-20 scale-75"
           src="/assets/video/bussl_neu.mp4"
           transition-speed="0.0001"
           :cover="false"
+          :track-scroll="true"
         />
       </div>
     </div>
