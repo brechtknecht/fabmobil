db8a524f7f149142b50b4d24063a16df93daf8ad
Felix Tesche <felix.tesche@gmail.com>
2023-07-15 14:09:40 +0200

    halfway responsive overlays

--- components/Base/BusOverlays.vue
+++ components/Base/BusOverlays.vue
@@ -1,15 +1,16 @@
 <template>
-  <div style="position: absolute !important">
+  <div class="absolute w-full" style="position: absolute">
     <BaseVideoOverlay
-      class="text-primary absolute z-50"
+      :class="overlayClasses"
+      class="text-primary bottom-0 sm:absolute z-50 w-full sm:w-auto"
       :scroll-percentage="scrollPercentage"
-      :position-x="20"
-      :position-y="20"
+      :position-x="getPositionX(20)"
+      :position-y="getPositionY(20)"
       :trigger-start="5"
       :trigger-end="20"
     >
-      <AppVideoOverlay>
-        <div class="p-8 bg-white rounded-md text-secondary max-w-sm">
+      <AppVideoOverlay class="relative left-0">
+        <div class="p-8 bg-white rounded-md text-secondary w-full sm:max-w-sm">
           <h2 class="font-headline text-title-1">Willkommen im Fabmobil!</h2>
           <p class="font-body text-body">Hier kannst du einiges Entdecken</p>
         </div>
@@ -17,15 +18,15 @@
     </BaseVideoOverlay>
 
     <BaseVideoOverlay
-      class="text-primary absolute z-50"
+      class="text-primary bottom-0 sm:absolute z-50 w-full sm:w-auto"
       :scroll-percentage="scrollPercentage"
-      :position-x="40"
-      :position-y="60"
+      :position-x="getPositionX(40)"
+      :position-y="getPositionY(60)"
       :trigger-start="22"
       :trigger-end="33"
     >
       <AppVideoOverlay>
-        <div class="p-8 bg-white rounded-md text-secondary max-w-sm">
+        <div class="p-8 bg-white rounded-md text-secondary w-full sm:max-w-sm">
           <h2 class="font-headline text-title-2">Technologien</h2>
           <p class="font-body text-body">
             Lasercutter, 3D-Drucker und haufen mehr Zeug gibts hier zum
@@ -36,15 +37,15 @@
     </BaseVideoOverlay>
 
     <BaseVideoOverlay
-      class="text-primary absolute z-50"
+      class="text-primary bottom-0 sm:absolute z-50 w-full sm:w-auto"
       :scroll-percentage="scrollPercentage"
-      :position-x="60"
-      :position-y="60"
+      :position-x="getPositionX(60)"
+      :position-y="getPositionY(60)"
       :trigger-start="42"
       :trigger-end="48"
     >
       <AppVideoOverlay>
-        <div class="p-8 bg-white rounded-md text-secondary max-w-sm">
+        <div class="p-8 bg-white rounded-md text-secondary w-full sm:max-w-sm">
           <p class="font-body text-body">
             Mit unseren Video-Tutorials bist du immer auf dem neuesten Stand.
             Schau mal rein!
@@ -62,6 +63,43 @@ export default {
     scrollPercentage: {
       type: Number,
       required: true,
+    },
+  },
+  data() {
+    return {
+      isSmallScreen: true,
+    }
+  },
+  computed: {
+    overlayClasses() {
+      return [
+        'text-primary',
+        'z-50',
+        this.isSmallScreen ? 'bottom-0 left-0 w-full' : 'absolute',
+      ]
+    },
+  },
+  mounted() {
+    if (process.client) {
+      this.isSmallScreen = window.innerWidth < 640
+      window.addEventListener('resize', this.checkWindowSize)
+      this.checkWindowSize()
+    }
+  },
+  beforeUnmount() {
+    if (process.client) {
+      window.removeEventListener('resize', this.checkWindowSize)
+    }
+  },
+  methods: {
+    getPositionX(x) {
+      return this.isSmallScreen ? 0 : x
+    },
+    getPositionY(y) {
+      return this.isSmallScreen ? 0 : y
+    },
+    checkWindowSize() {
+      this.isSmallScreen = window.innerWidth < 640
     },
   },
 }
--- components/Modules/Intro.vue
+++ components/Modules/Intro.vue
@@ -1,32 +1,40 @@
 <template>
   <div
-    class="intro-section flex flex-col bg-purple tertiary items-center justify-center h-fit py-24"
+    class="intro-section flex flex-col bg-cover bg-no-repeat bg-purple-600 items-center justify-center h-auto sm:h-full py-24"
+    :style="{
+      backgroundImage: 'url(/assets/img/frontpage/background-layer.png)',
+    }"
   >
-    <div class="flex flex-row h-fit">
-      <!-- <BaseRough /> -->
+    <div
+      class="flex flex-col sm:flex-row w-full justify-center items-center h-auto sm:h-full"
+    >
       <h1
-        class="font-headline text-large-title text-secondary w-full text-center max-w-[40vw] text-left py-8"
+        class="font-bold text-3xl sm:text-5xl text-black w-full text-center sm:text-left py-8"
       >
         Das Fabmobil ist eine mobile Digitalwerkstatt für Sachsen
       </h1>
     </div>
-    <div class="flex flex-row h-fit">
+    <div
+      class="flex flex-col sm:flex-row w-full justify-center items-center h-auto sm:h-full"
+    >
       <p
-        class="font-body text-headline leading-relaxed text-secondary w-full text-center max-w-[40vw] text-left py-8"
+        class="font-normal text-xl sm:text-2xl leading-relaxed text-white w-full text-center sm:text-left py-8"
       >
         Der mit Digitaltechnik und Maschinen ausgestattete Doppeldeckerbus
         bietet Kindern und Jugendlichen die Möglichkeit allerlei Cerative
         Technologies zu entdecken.
       </p>
     </div>
-    <div class="flex flex-col sm:flex-row h-fit justify-center items-center">
+    <div
+      class="flex flex-col sm:flex-row h-auto sm:h-full justify-center items-center"
+    >
       <h1
-        class="font-headline text-large-title text-secondary w-full text-center"
+        class="font-bold text-3xl sm:text-5xl text-white w-full text-center sm:text-left"
       >
         Kostenlose Workshops
       </h1>
       <p
-        class="font-body text-headline leading-relaxed text-secondary w-full text-left p-16"
+        class="font-normal text-xl sm:text-2xl leading-relaxed text-white w-full text-left p-16"
       >
         Seid ihr eine Schule, ein Jugendclub oder ein soziokulturelles Zentrum
         in Sachsen? Dann meldet euch bei uns für einen Workshop. Die sind für
@@ -37,10 +45,3 @@
 </template>
 
 <script setup lang="ts"></script>
-
-<style lang="scss" scoped>
-.intro-section {
-  background: url('/assets/img/frontpage/background-layer.png');
-  background-size: cover;
-}
-</style>
