859a56df3f379a6e7692c18b3376d80eaf38ac42
Felix Tesche <felixtesche@unknown26b4fe6bc52b.localdomain>
2023-09-20 15:15:34 +0200

    semi-clean version for firefox

--- components/Modules/BusNoWebCodecs.vue
+++ components/Modules/BusNoWebCodecs.vue
@@ -3,12 +3,14 @@
     <div class="container mx-auto">
       <div class="scrolly-video-container relative">
         <BaseBusOverlays
-          class="appe w-full h-full relative z-40"
+          v-show="isInViewport"
+          class="sticky w-full h-full z-40"
           :scroll-percentage="progressPercentage"
         />
+
         <video-player
           ref="videoPlayer"
-          class="video-player-box"
+          class="sticky video-player-box"
           src="/assets/video/sequence-compressed.mp4"
           poster="/your-path/poster.jpg"
           controls
@@ -16,7 +18,9 @@
           :loop="true"
           :volume="0.6"
         />
-        <pre class="sticky top-32 text-white">{{ progressPercentage }}</pre>
+        <pre class="sticky top-32 text-white"
+          >{{ progressPercentage }} — {{ isInViewport }}</pre
+        >
       </div>
     </div>
   </div>
@@ -26,22 +30,43 @@
 import { VideoPlayer } from '@videojs-player/vue'
 
 export default {
-  components: {
-    VideoPlayer,
-  },
   data() {
     return {
+      isInViewport: false,
       progressPercentage: 0,
+      observer: null, // Define observer here
     }
   },
   mounted() {
     this.$nextTick(() => {
+      const containerElement = this.$el.querySelector(
+        '.scrolly-video-container'
+      )
       const videoElement = this.$refs.videoPlayer.$el.querySelector('video')
+
       videoElement.addEventListener('timeupdate', () => {
         this.progressPercentage =
           (videoElement.currentTime / videoElement.duration) * 100
       })
+
+      // Create a new Intersection Observer
+      this.observer = new IntersectionObserver(
+        (entries) => {
+          // If the element is in the viewport, set isInViewport to true, else false
+          this.isInViewport = entries[0].intersectionRatio >= 0.5
+        },
+        {
+          threshold: [0.5], // 50% visibility
+        }
+      )
+
+      // Start observing the container element
+      this.observer.observe(containerElement)
     })
+  },
+  beforeUnmount() {
+    // Stop observing when the component is destroyed
+    this.observer.disconnect()
   },
 }
 </script>
