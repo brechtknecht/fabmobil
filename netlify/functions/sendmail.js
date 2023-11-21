9ec54ac05c5e7f78c80f668ef23d5ab4e4dbfc24
Felix Tesche <felix.tesche@gmail.com>
2023-11-21 02:15:35 +0100

    added sendmail netlify function

---
+++ netlify/functions/sendmail.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer')
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method Not Allowed' }
+  }
+
+  const { name, contactperson, request } = JSON.parse(event.body)
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'fabmobiltest@gmail.com',
+      pass: 'xn4xmhuaFK9uom9dcp343qU_',
+    },
+  })
+
+  const mailOptions = {
+    from: 'your_gmail_address@gmail.com',
+    to: 'receiver_email@example.com',
+    subject: 'New Contact Form Submission',
+    text: `Name: ${name}\nContact Person: ${contactperson}\nRequest: ${request}`,
+  }
+
+  try {
+    await transporter.sendMail(mailOptions)
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Email sent successfully' }),
+    }
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Failed to send email' }),
+    }
+  }
+}
