1301e662e796a4a79ccbcf9a84fcc8d244e9e3ca
Felix Tesche <felix.tesche@gmail.com>
2023-11-21 02:39:44 +0100

    updated buttons onmail

--- components/Modals/Contact.vue
+++ components/Modals/Contact.vue
@@ -63,13 +63,6 @@
                   Wir werden uns zeitnah bei dir melden.
                 </p>
               </div>
-              <button
-                type="button"
-                class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-secondary text-primary hover:bg-gray-50 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                @click="closeModal"
-              >
-                Schließen
-              </button>
             </div>
           </div>
         </div>
@@ -124,6 +117,7 @@
         </div>
         <div class="bg-secondary px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
           <button
+            v-if="!isSubmitted"
             type="button"
             class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green text-base font-medium text-black hover:bg-green-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-dark sm:ml-3 sm:w-auto sm:text-sm"
             @click="submitForm"
@@ -131,11 +125,20 @@
             Absenden
           </button>
           <button
+            v-if="!isSubmitted"
             type="button"
             class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-secondary text-primary hover:bg-gray-50 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
             @click="closeModal"
           >
             Abbrechen
+          </button>
+          <button
+            v-if="isSubmitted"
+            type="button"
+            class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-secondary text-primary hover:bg-gray-50 hover:text-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+            @click="closeModal"
+          >
+            Schließen
           </button>
         </div>
       </div>
