001fb546c577b36ddf30ac263e7a4558c18ef88e
Felix Tesche <felix.tesche@gmail.com>
2023-11-21 02:02:32 +0100

    updated sponsor gallery

--- components/Modules/Sponsors.vue
+++ components/Modules/Sponsors.vue
@@ -1,16 +1,25 @@
 <template>
-  <div class="grid-content bg-black">
+  <div class="bg-black p-4">
+    <div class="container mx-auto px-4">
+      <h1 class="text-4xl font-headline mb-6 px-32 pt-32 max-w-xl">
+        Gefördert durch
+      </h1>
+    </div>
     <div
-      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 mx-auto justify-center max-w-[1720px] space-x-16 px-16"
+      class="flex flex-wrap justify-center gap-32 mx-auto max-w-[1720px] py-20"
     >
       <a
         v-for="(sponsor, index) in sponsors"
         :key="index"
         :href="sponsor.url"
         target="_blank"
-        class="sponsor flex justify-center items-center"
+        class="flex-none w-full sm:w-full md:w-1/2 lg:w-1/3 flex justify-center items-center"
       >
-        <img :src="sponsor.image.url" alt="Sponsor logo" class="py-4 w-full" />
+        <img
+          :src="sponsor.image.url"
+          alt="Sponsor logo"
+          class="h-64 object-contain mx-auto py-4"
+        />
       </a>
     </div>
   </div>
