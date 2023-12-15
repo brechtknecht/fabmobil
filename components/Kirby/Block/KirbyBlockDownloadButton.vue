b64fdc5589f801deb96d1e6d3ab5efc6c646d698
Felix Tesche <felix.tesche@gmail.com>
2023-12-15 15:50:19 +0100

    added download button

--- components/Kirby/Block/KirbyBlockDownloadButton.vue
+++ components/Kirby/Block/KirbyBlockDownloadButton.vue
@@ -1,9 +1,9 @@
 <template>
-  {{ props }}
   <div class="flex flex-row gap-4">
     <a
       target="_blank"
-      class="button transform rounded-md h-fit flexitems-start justify-items-start flex-col md:flex-row w-64 py-1.5 px-4 cursor-pointe"
+      :href="fileData.url"
+      class="button transform h-fit flexitems-start justify-items-start flex-col md:flex-row w-64 py-1.5 px-4 cursor-pointe border-2"
       :class="{
         'bg-black': props.block.content.dark == 'true',
         'bg-white': props.block.content.dark !== 'true',
@@ -35,12 +35,33 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
+import { ref, watch } from 'vue'
 import type { KirbyBlock } from '#nuxt-kql'
 
 const props = defineProps<{
   block: KirbyBlock<'downloadbutton'>
 }>()
+
+const { data } = await useKql({
+  query: `file("${props.block.content.file}")`,
+  select: {
+    id: true,
+    url: true,
+  },
+})
+
+const fileData = ref(null)
+
+// Watch for changes in data.value and update fileData accordingly
+watch(
+  () => data.value,
+  (newValue) => {
+    fileData.value = newValue?.result
+  },
+  { immediate: true }
+)
+
+console.log(fileData)
 </script>
 
 <style scoped>
